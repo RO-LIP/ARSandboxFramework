09a4225127311ac895061131485e750fc4852dce
sINFalgebh <alexander.gebhardt@stud.fh-rosenheim.de>
2019-10-26 12:32:20 +0200

    Added script for multi monitor support:
    Added script for multi monitor detection and activation to main camera

---
+++ TestingProject/RealSenseUnity/Assets/Scripts/MultiMonitorActivator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MultiMonitorActivator : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Number of connected displays: " + Display.displays.Length);
+        if (Display.displays.Length > 1)
+        {
+            Debug.Log("Activating Display 2");
+            Display.displays[1].Activate();
+        }
+    }
+}
