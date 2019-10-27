a986a4df649f8df1147ab13990c5b2a09f9dad9a
sINFalgebh <alexander.gebhardt@stud.fh-rosenheim.de>
2019-10-27 19:40:46 +0100

    added rudementary settings ui to 2nd monitor:
    Made splatmap parameters dynamic
    Enable 2nd camera only when running in multi monitor mode
    Added rudementary UI for manipulating splatmap parameters to 2nd monitor

--- TestingProject/RealSenseUnity/Assets/Scripts/GetDistanceTest.cs
+++ TestingProject/RealSenseUnity/Assets/Scripts/GetDistanceTest.cs
@@ -5,6 +5,33 @@ public class GetDistanceTest : MonoBehaviour
     public Terrain terrain;
     public RsDevice rsDevice;
 
+    public float noiseCutoff;
+
+    public float texture0Cutoff;
+    public float texture1Cutoff;
+    public float texture2Cutoff;
+
+    public float textureTransitionSize;
+
+    public void SetTexture0Cutoff(float newValue)
+    {
+        texture0Cutoff = newValue;
+    }
+
+    public void SetTexture1Cutoff(float newValue)
+    {
+        texture1Cutoff = newValue;
+    }
+
+    public void SetTexture2Cutoff(float newValue)
+    {
+        texture2Cutoff = newValue;
+    }
+
+    public void SetTextureTransitionSize(float newValue)
+    {
+        textureTransitionSize = newValue;
+    }
 
     void OnNewSample(Frame frame)
     {
@@ -25,7 +52,7 @@ public class GetDistanceTest : MonoBehaviour
                         for(int x = 0; x < depthFrame.Height; x++)
                         {
                             float currHeight = 1.2f - depthFrame.GetDistance(y, x);
-                            if(currHeight > 0.9f)
+                            if(currHeight > noiseCutoff)
                             {
                                 Map[x, y] = prevHeight;
                             }
@@ -38,7 +65,7 @@ public class GetDistanceTest : MonoBehaviour
                             float height = Map[x,y] * 10;
                             
                             // determine the mix of textures 1, 2 &amp; 3 to use 
-                            // (using a vector3, since it can be lerped &amp; normalized)
+                            // (using a vector3, since it can be lerped and normalized)
                             // Normalise x/y coordinates to range 0-1 
 
                             float y_01 = (float)y / (float)dfHeight;
@@ -46,69 +73,57 @@ public class GetDistanceTest : MonoBehaviour
 
                             // Setup an array to record the mix of texture weights at this point
                             float[] splatWeights = new float[4];
-                            
-                            if(x == 100 && y == 100)
-                            {
-                                Debug.Log(height);
-                                Debug.Log(Map[100,100]);
-                            }
 
-                       
-                            if(height < 1)
+                            if(height < texture0Cutoff)
                             {
                                 splatWeights[0] = 1;
                             }
 
-                            if (height >= 1 && height < 2)
+                            if (height >= texture0Cutoff && height < texture1Cutoff)
                             {
                                 splatWeights[1] = 1;
                             }
-                            if (height >= 2f && height < 2.5f)
+                            if (height >= texture1Cutoff && height < texture1Cutoff + textureTransitionSize)
                             {
-                                var diff = height - 2f;
+                                var diff = height - texture1Cutoff;
                                 splatWeights[1] = 0.5f - diff;
                                 splatWeights[2] = 0.5f + diff;
                             }
 
-                            if (height >= 2.5f && height < 3.5f)
+                            if (height >= texture1Cutoff + textureTransitionSize && height < texture2Cutoff)
                             {
                                 splatWeights[2] = 1;
                             }
-                            if (height >= 3.5f && height < 4)
+                            if (height >= texture2Cutoff && height < texture2Cutoff + textureTransitionSize)
                             {
-                                var diff = height - 3.5f;
+                                var diff = height - texture2Cutoff;
                                 splatWeights[2] = 0.5f - diff;
                                 splatWeights[3] = 0.5f + diff;
                             }
                           
-                            if (height >= 4)
+                            if (height >= texture2Cutoff + textureTransitionSize)
                             {
                                 splatWeights[3] = 1;
                             }
                            
-                      
-
                             // Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
                             float z = splatWeights[0] + splatWeights[1] + splatWeights[2] + splatWeights[3];
 
                             // Loop through each terrain texture
                             for (int i = 0; i < 4; i++)
                             {
-
                                 // Normalize so that sum of all texture weights = 1
                                 splatWeights[i] /= z;
 
                                 // Assign this point to the splatmap array
                                 splatmapData[x, y, i] = splatWeights[i];
-                                // N
-
                             }
                         }
                     }
                     terrain.terrainData.heightmapResolution = dfWidth;
-                        terrain.terrainData.size = new Vector3(dfWidth, 100, dfHeight);
-                        terrain.terrainData.SetHeightsDelayLOD(0, 0, Map);
-                        terrain.terrainData.SetAlphamaps(0, 0, splatmapData);
+                    terrain.terrainData.size = new Vector3(dfWidth, 100, dfHeight);
+                    terrain.terrainData.SetHeightsDelayLOD(0, 0, Map);
+                    terrain.terrainData.SetAlphamaps(0, 0, splatmapData);
                 }
             }
         }
--- TestingProject/RealSenseUnity/Assets/Scripts/MultiMonitorActivator.cs
+++ TestingProject/RealSenseUnity/Assets/Scripts/MultiMonitorActivator.cs
@@ -2,12 +2,15 @@
 
 public class MultiMonitorActivator : MonoBehaviour
 {
+    public bool useSecondMonitor;
+    public Camera display2Camera;
     void Start()
     {
         Debug.Log("Number of connected displays: " + Display.displays.Length);
-        if (Display.displays.Length > 1)
+        if (Display.displays.Length > 1 && useSecondMonitor)
         {
             Debug.Log("Activating Display 2");
+            display2Camera.gameObject.SetActive(true);
             Display.displays[1].Activate();
         }
     }
