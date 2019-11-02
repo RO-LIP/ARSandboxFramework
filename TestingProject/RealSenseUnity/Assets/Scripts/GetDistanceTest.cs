a45925e32d67a5fe12115376667bdcaca8ce4218
sInfpastad <patrick.stadler@stud.fh-rosenheim.de>
2019-11-02 19:50:05 +0100

    recreate multithreading

--- TestingProject/RealSenseUnity/Assets/Scripts/GetDistanceTest.cs
+++ TestingProject/RealSenseUnity/Assets/Scripts/GetDistanceTest.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using Intel.RealSense;
+using System.Threading;
+using System.Collections.Concurrent;
+
 public class GetDistanceTest : MonoBehaviour
 {
     public Terrain terrain;
+    public RsProcessingPipe processingPipe;
     public RsDevice rsDevice;
+    ConcurrentQueue<float[,]> heightMapQueue = new ConcurrentQueue<float[,]>();
+    ConcurrentQueue<float[,]> tempheightMapQueue = new ConcurrentQueue<float[,]>();
+    ConcurrentQueue<float[,,]> splatMapQueue = new ConcurrentQueue<float[,,]>();
+    DepthFrame currentDepthFrame;
+    int width;
+    int height;
+    float[,] Map;
+
 
     public float noiseCutoff;
 
@@ -61,76 +73,144 @@ public class GetDistanceTest : MonoBehaviour
                                 Map[x, y] = prevHeight  = 1.2f - depthFrame.GetDistance(y,x);
                             }
 
-                            // SPLATTIN
-                            float height = Map[x,y] * 10;
-                            
-                            // determine the mix of textures 1, 2 &amp; 3 to use 
-                            // (using a vector3, since it can be lerped and normalized)
-                            // Normalise x/y coordinates to range 0-1 
-
-                            float y_01 = (float)y / (float)dfHeight;
-                            float x_01 = (float)x / (float)dfWidth;
-
-                            // Setup an array to record the mix of texture weights at this point
-                            float[] splatWeights = new float[4];
-
-                            if(height < texture0Cutoff)
-                            {
-                                splatWeights[0] = 1;
-                            }
-
-                            if (height >= texture0Cutoff && height < texture1Cutoff)
-                            {
-                                splatWeights[1] = 1;
-                            }
-                            if (height >= texture1Cutoff && height < texture1Cutoff + textureTransitionSize)
-                            {
-                                var diff = height - texture1Cutoff;
-                                splatWeights[1] = 0.5f - diff;
-                                splatWeights[2] = 0.5f + diff;
-                            }
-
-                            if (height >= texture1Cutoff + textureTransitionSize && height < texture2Cutoff)
-                            {
-                                splatWeights[2] = 1;
-                            }
-                            if (height >= texture2Cutoff && height < texture2Cutoff + textureTransitionSize)
-                            {
-                                var diff = height - texture2Cutoff;
-                                splatWeights[2] = 0.5f - diff;
-                                splatWeights[3] = 0.5f + diff;
-                            }
-                          
-                            if (height >= texture2Cutoff + textureTransitionSize)
-                            {
-                                splatWeights[3] = 1;
-                            }
-                           
-                            // Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
-                            float z = splatWeights[0] + splatWeights[1] + splatWeights[2] + splatWeights[3];
-
-                            // Loop through each terrain texture
-                            for (int i = 0; i < 4; i++)
-                            {
-                                // Normalize so that sum of all texture weights = 1
-                                splatWeights[i] /= z;
-
-                                // Assign this point to the splatmap array
-                                splatmapData[x, y, i] = splatWeights[i];
-                            }
                         }
                     }
-                    terrain.terrainData.heightmapResolution = dfWidth;
-                    terrain.terrainData.size = new Vector3(dfWidth, 100, dfHeight);
-                    terrain.terrainData.SetHeightsDelayLOD(0, 0, Map);
-                    terrain.terrainData.SetAlphamaps(0, 0, splatmapData);
+
+                    height = dfHeight;
+                    width = dfWidth;
+                    if (tempheightMapQueue.IsEmpty)
+                    {
+                        tempheightMapQueue.Enqueue(Map);
+                    }
+                    if (heightMapQueue.IsEmpty)
+                    {
+                        heightMapQueue.Enqueue(Map);
+                    }
+
+                 
                 }
             }
         }
     }
 
+
+    public void generateSplatmapData()
+    {
+        while (true)
+        {
+            if (tempheightMapQueue.IsEmpty)
+            {
+                Thread.Sleep(10);
+            }
+            else
+            {
+                float[,] heightmap = new float[width, width];
+                tempheightMapQueue.TryDequeue(out heightmap);
+                float[,,] splatmapData = new float[width, width, 4];
+                for (int y = 0; y < width; y++)
+                {
+                    for (int x = 0; x < height; x++)
+                    {
+                        float height = heightmap[x, y] * 10;
+                        
+
+                        // determine the mix of textures 1, 2 &amp; 3 to use 
+                        // (using a vector3, since it can be lerped and normalized)
+                        // Normalise x/y coordinates to range 0-1 
+
+                        float y_01 = (float)y / (float)height;
+                        float x_01 = (float)x / (float)width;
+
+                        // Setup an array to record the mix of texture weights at this point
+                        float[] splatWeights = new float[4];
+
+                        if (height < texture0Cutoff)
+                        {
+                            splatWeights[0] = 1;
+                        }
+
+                        if (height >= texture0Cutoff && height < texture1Cutoff)
+                        {
+                            splatWeights[1] = 1;
+                        }
+                        if (height >= texture1Cutoff && height < texture1Cutoff + textureTransitionSize)
+                        {
+                            var diff = height - texture1Cutoff;
+                            splatWeights[1] = 0.5f - diff;
+                            splatWeights[2] = 0.5f + diff;
+                        }
+
+                        if (height >= texture1Cutoff + textureTransitionSize && height < texture2Cutoff)
+                        {
+                            splatWeights[2] = 1;
+                        }
+                        if (height >= texture2Cutoff && height < texture2Cutoff + textureTransitionSize)
+                        {
+                            var diff = height - texture2Cutoff;
+                            splatWeights[2] = 0.5f - diff;
+                            splatWeights[3] = 0.5f + diff;
+                        }
+
+                        if (height >= texture2Cutoff + textureTransitionSize)
+                        {
+                            splatWeights[3] = 1;
+                        }
+
+                        // Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
+                        float z = splatWeights[0] + splatWeights[1] + splatWeights[2] + splatWeights[3];
+
+                        // Loop through each terrain texture
+                        for (int i = 0; i < 4; i++)
+                        {
+                            // Normalize so that sum of all texture weights = 1
+                            splatWeights[i] /= z;
+
+                            // Assign this point to the splatmap array
+                            splatmapData[x, y, i] = splatWeights[i];
+                        }
+                    }
+                }
+
+                if (splatMapQueue.IsEmpty)
+                {
+                    splatMapQueue.Enqueue(splatmapData);
+                }
+
+            }
+        }
+    }
+
+
     private void Start()
     {
-        rsDevice.OnNewSample += OnNewSample;
+        processingPipe.OnNewSample += OnNewSample;
+        Thread thread = new Thread(generateSplatmapData);
+        thread.Start();
+
     }
+
+    private void Update()
+    {
+        if (!heightMapQueue.IsEmpty)
+        {
+            float[,] heightmap = new float[width, width];
+            heightMapQueue.TryDequeue(out heightmap);
+
+            terrain.terrainData.heightmapResolution = width;
+            terrain.terrainData.size = new Vector3(width, 100, width);
+            terrain.terrainData.SetHeightsDelayLOD(0, 0, heightmap);
+        }
+
+        if (!splatMapQueue.IsEmpty)
+        {
+            float[,,] splatmapData = new float[width, width, 4];
+            splatMapQueue.TryDequeue(out splatmapData);
+            terrain.terrainData.SetAlphamaps(1, 1, splatmapData);
+        }
+
+
+    }
+
+
+
 }
