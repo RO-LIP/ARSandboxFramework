2b4d8e1b643a2374e6d802d53f10e7f71e62b903
sInfpastad <patrick.stadler@stud.fh-rosenheim.de>
2019-11-03 11:03:21 +0100

    close application with escape menu

---
+++ TestingProject/RealSenseUnity/Assets/CloseApplication.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseApplication : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
