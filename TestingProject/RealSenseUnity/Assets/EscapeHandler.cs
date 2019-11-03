9e1cbcfe23a38c9a0108045650d6e2ba7d0e53b7
sINFalgebh <alexander.gebhardt@stud.fh-rosenheim.de>
2019-11-03 11:53:02 +0100

    Improved ESC menu behaviour:
    Removed unneccesary code
    Changed key detection to key down so one press only toggles the menu once

--- TestingProject/RealSenseUnity/Assets/EscapeHandler.cs
+++ TestingProject/RealSenseUnity/Assets/EscapeHandler.cs
@@ -1,20 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EscapeHandler : MonoBehaviour
 {
     public GameObject uiPanel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
             if(uiPanel.active)
             {
                 uiPanel.SetActive(false);
