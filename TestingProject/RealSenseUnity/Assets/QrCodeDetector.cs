60e13ff0516f4086fba0c2c4158b785956c29602
sINFalgebh <alexander.gebhardt@stud.fh-rosenheim.de>
2019-12-07 22:08:14 +0100

    Correcting to make QR code detection work:
    Corrected MatType from CV_8U to CV_8UC3
    Corrected camera stream settings to add color stream in bgr8 format
    Improved code formating

--- TestingProject/RealSenseUnity/Assets/QrCodeDetector.cs
+++ TestingProject/RealSenseUnity/Assets/QrCodeDetector.cs
@@ -1,42 +1,41 @@
 ﻿
-    using UnityEngine;
-    using System.Collections;
-    using OpenCvSharp;
-    using UnityEngine.UI;
-    using Intel.RealSense;
+using UnityEngine;
+using System.Collections;
+using OpenCvSharp;
+using UnityEngine.UI;
+using Intel.RealSense;
 
-    public class QrCodeDetector : MonoBehaviour
-    {
+public class QrCodeDetector : MonoBehaviour
+{
     private QRCodeDetector qrCodeDetector;
     private bool isDetecting = false;
-        public RsProcessingPipe processingPipe;
-        
-        void OnNewSample(Frame frame)
-        {
+    public RsProcessingPipe processingPipe;
+
+    void OnNewSample(Frame frame)
+    {
         if (isDetecting) return;
 
         using (var frameSet = frame.AsFrameSet())
+        {
+            using (var colorFrame = frameSet.ColorFrame)
             {
-           
-                using (var colorFrame = frameSet.ColorFrame)
-                {
                 if (colorFrame == null) return;
                 // set isDetecting true to avoid crashes 
                 isDetecting = true;
                 int cfWidth = colorFrame.Width;
-                    int cfHeight = colorFrame.Height;
+                int cfHeight = colorFrame.Height;
                 // transform realsense frame into cv mat format
-                using (var image = new Mat(cfWidth, cfHeight, MatType.CV_8U, colorFrame.Data))
+                using (var image = new Mat(cfHeight, cfWidth, MatType.CV_8UC3, colorFrame.Data))
                 {
                     using (qrCodeDetector = new QRCodeDetector())
                     {
+                        // Show generated Image in separate window
+                        // Window.ShowImages(image);
                         // check frame if qrcode is existing
                         bool detected = qrCodeDetector.Detect(image, out var points);
-                        
-                        Debug.Log(detected);
+
                         if (detected)
                         {
-                            Debug.Log(detected);
                             using (var straightQrCode = new Mat())
                             {
                                 qrCodeDetector.Decode(image, points);
@@ -46,33 +45,15 @@
                         }
                         isDetecting = false;
                     }
-
                 }
-                    
             }
-
-            }
-
         }
-
-
-
-
-
-        private void Start()
-        {
-            processingPipe.OnNewSample += OnNewSample;
-
-
-        }
-
-        private void Update()
-        {
-
-        }
-
-
-
     }
+
+    private void Start()
+    {
+        processingPipe.OnNewSample += OnNewSample;
+    }
+}
 
 
