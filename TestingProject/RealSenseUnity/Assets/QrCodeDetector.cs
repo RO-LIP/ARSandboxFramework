a50ac968ee0a5e2ce836d102345e8174c125987f
Christian Kink <chris.kink@live.de>
2019-12-07 18:44:08 +0100

    commit for alex to help me out with colored frames

--- TestingProject/RealSenseUnity/Assets/QrCodeDetector.cs
+++ TestingProject/RealSenseUnity/Assets/QrCodeDetector.cs
@@ -8,25 +8,46 @@
     public class QrCodeDetector : MonoBehaviour
     {
     private QRCodeDetector qrCodeDetector;
-
+    private bool isDetecting = false;
         public RsProcessingPipe processingPipe;
-
+        
         void OnNewSample(Frame frame)
         {
+        if (isDetecting) return;
+
         using (var frameSet = frame.AsFrameSet())
             {
            
                 using (var colorFrame = frameSet.ColorFrame)
                 {
                 if (colorFrame == null) return;
-                
-                //Debug.Log(depthFrame);
-                //Gray scale image
+                isDetecting = true;
                 int cfWidth = colorFrame.Width;
                     int cfHeight = colorFrame.Height;
-                    Mat image = new Mat (cfWidth, cfHeight, MatType.CV_16UC1, colorFrame.Data);
-                    bool detected = qrCodeDetector.Detect(image, out var points);
-                    Debug.Log(detected);
+                using (var image = new Mat(cfWidth, cfHeight, MatType.CV_8U, colorFrame.Data))
+                {
+                    using (qrCodeDetector = new QRCodeDetector())
+                    {
+                        bool detected = qrCodeDetector.Detect(image, out var points);
+                        
+                        isDetecting = false;
+                        Debug.Log(detected);
+                        if (detected)
+                        {
+                            Debug.Log(detected);
+                            using (var straightQrCode = new Mat())
+                            {
+                                qrCodeDetector.Decode(image, points);
+                                var decodedString = qrCodeDetector.Decode(image, points, straightQrCode);
+                                Debug.Log(decodedString);
+                            }
+                        }
+                        
+
+                    }
+
+                }
+                    
             }
 
             }
@@ -39,11 +60,8 @@
 
         private void Start()
         {
-        using (qrCodeDetector = new QRCodeDetector())
-        {
             processingPipe.OnNewSample += OnNewSample;
 
-        }
 
         }
 
