b9a32beb89043a4604c144fde853f64d20bd3052
sINFalgebh <alexander.gebhardt@stud.fh-rosenheim.de>
2020-01-13 17:16:07 +0100

    bugfixes and code cleanup:
    Removed unused imports
    Removed unused scripts
    Opening settings with ESC now works again
    Resetting sphere position using UI now works again

--- TestingProject/RealSenseUnity/Assets/Scripts/EventManager.cs
+++ TestingProject/RealSenseUnity/Assets/Scripts/EventManager.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
-using System.Collections;
 using System.Collections.Generic;
 
 public class EventManager : MonoBehaviour {
--- TestingProject/RealSenseUnity/Assets/Scripts/SpawnHandler.cs
+++ TestingProject/RealSenseUnity/Assets/Scripts/SpawnHandler.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Events;
-using System.Collections;
 
 public class SpawnHandler : MonoBehaviour
 {
