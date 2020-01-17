4fe0fdee0d99d259d865162887f35f8b9019eaa6
sINFalgebh <alexander.gebhardt@stud.fh-rosenheim.de>
2020-01-17 21:27:47 +0100

    Added moving tiger and switchable light scenes:
    Added tiger that walks on terrain using the navmesh
    Added switch day/night capability
    Added a toggle switch for day and night to UI
    Tweaked tiger walk animation looping to be seamless
    Removed unused script

---
+++ RealSenseUnity/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public Light directionaLight;
+    public Light[] ambientLights;
+
+    public Color32 directionalLightColorDay;
+    public Color32 directionalLightColorNight;
+
+    public Color32 ambientLightColorDay;
+    public Color32 ambientLightColorNight;
+
+    private bool isDay = true;
+
+    private void Start()
+    {
+        SetDirectionalLightColor(directionalLightColorDay);
+        SetAmbientLightColor(ambientLightColorDay);
+    }
+
+    public void ToggleNightDay()
+    {
+        isDay = !isDay;
+        if (isDay)
+        {
+            SetDirectionalLightColor(directionalLightColorDay);
+            SetAmbientLightColor(ambientLightColorDay);
+        }
+        else
+        {
+            SetDirectionalLightColor(directionalLightColorNight);
+            SetAmbientLightColor(ambientLightColorNight);
+        }
+    }
+
+    private void SetDirectionalLightColor(Color color)
+    {
+        directionaLight.color = color;
+    }
+
+    private void SetAmbientLightColor(Color color)
+    {
+        foreach (Light ambientLight in ambientLights)
+        {
+            ambientLight.color = color;
+            if (ColorIsBlack(color))
+            {
+                ambientLight.gameObject.SetActive(false);
+            } else
+            {
+                ambientLight.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private bool ColorIsBlack(Color color)
+    {
+        float channelSum = color.r + color.g + color.b;
+        return channelSum == 0;
+    }
+}
---
+++ RealSenseUnity/Assets/Scripts/TigerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class TigerMovement : MonoBehaviour
+{
+    public Camera NavAgentDestinationSelectionCamera;
+
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+        navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            Ray ray = NavAgentDestinationSelectionCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                navMeshAgent.destination = hitInfo.point;
+        }
+
+        bool isMoving = navMeshAgent.velocity.magnitude > 0.1f;
+        animator.SetBool("isMoving", isMoving);
+    }
+}
